47ccde362ed2e489fbb84729914a775ae0dc6399
Samet <samettt18@hotmail.com.tr>
2021-01-14 20:35:20 +0300

    CMSProject.Data oluşturuldu

---
+++ CMSProject.Data/SeedData/SeedPage.cs
@@ -0,0 +1,22 @@
+﻿using CMSProject.Entity.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMSProject.Data.SeedData
+{
+    public class SeedPage : IEntityTypeConfiguration<Page>
+    {
+        public void Configure(EntityTypeBuilder<Page> builder)
+        {
+            // Database'de tek tek manuel olarak girmemek adı altında tohumlama yapılarak verileri database de bulunan Page tablosuna gönderdik.
+            builder.HasData(
+                 new Page { Id = 1, Title = "Home", Content = "Home Page", Slug = "home-page", Sorting = 100 },
+                 new Page { Id = 2, Title = "About Us", Content = "About Us Page", Slug = "about-page", Sorting = 100 },
+                 new Page { Id = 3, Title = "Services", Content = "Services Page", Slug = "service-page", Sorting = 100 },
+                 new Page { Id = 4, Title = "Contact Us", Content = "Conctact Us Page", Slug = "contact-page", Sorting = 100 });
+        }
+    }
+}
