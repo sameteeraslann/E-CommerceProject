0268e85fdfd48cc4bfddf0d3b365ee9b1cdc2fb0
Samet <samettt18@hotmail.com.tr>
2021-01-14 20:45:52 +0300

    Context klasörü eklendi.

---
+++ CMSProject.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using CMSProject.Data.SeedData;
+using CMSProject.Entity.Entities.Concrete;
+using CMSProject.Map.Mapping.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMSProject.Data.Context
+{
+    public class ApplicationDbContext: IdentityDbContext <AppUser>
+    {
+        // IdentyDbContext<AppUser> => AppUser Entity katmanında IdentytyUser olarak belirlendiğnden dolayı Context işlemleri gerçekleşti ve bir daha burada "DbSet<AppUser>" olarak ekstra belirtmedik.
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Page>  Pages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new CategoryMap());
+            builder.ApplyConfiguration(new AppUserMap());
+            builder.ApplyConfiguration(new PageMap());
+            builder.ApplyConfiguration(new ProductMap());
+
+            builder.ApplyConfiguration(new SeedPage());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
