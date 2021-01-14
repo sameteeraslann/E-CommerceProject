7514aad48d6569555e7dff9009f326ec4bcc5884
Samet <samettt18@hotmail.com.tr>
2021-01-14 18:37:26 +0300

    Abstract Klasörü Eklendi

---
+++ CMSProject.Map/Mapping/Abstract/BaseMap.cs
@@ -0,0 +1,21 @@
+﻿using CMSProject.Entity.Entities.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMSProject.Map.Mapping.Abstract
+{
+    public abstract class BaseMap<T> : IEntityTypeConfiguration<T> where T : class, IBaseEntity
+    {
+        // IEntityTypeConfiguration => Varlıkları, özelliklerini ve varlık işlemlerini veri tabanında eşitlemek için kullanılır.
+        public void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.Property(x => x.CreateDate).IsRequired(true);
+            builder.Property(x => x.UpdateDate).IsRequired(false);
+            builder.Property(x => x.DeleteDate).IsRequired(false);
+            builder.Property(x => x.Status).IsRequired(true);
+        }
+    }
+}
